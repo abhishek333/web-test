8227aa3d501b59cf7ed6347db6b2f3edef62c4e9
abhishek333 <as.nayak1990@gmail.com>
2014-12-09 07:37:21 +0530

    session support added

--- src/main/java/org/asn/web_test/api/HttpTestImpl.java
+++ src/main/java/org/asn/web_test/api/HttpTestImpl.java
@@ -6,6 +6,7 @@ package org.asn.web_test.api;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.text.MessageFormat;
 import java.util.Properties;
 
 import org.apache.http.HttpEntity;
@@ -14,18 +15,15 @@ import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.CookieStore;
 import org.apache.http.client.ResponseHandler;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.protocol.ClientContext;
+import org.apache.http.client.protocol.HttpClientContext;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.protocol.BasicHttpContext;
-import org.apache.http.protocol.HTTP;
-import org.apache.http.protocol.HttpContext;
 import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -38,23 +36,26 @@ import com.fasterxml.jackson.databind.ObjectMapper;
  */
 public class HttpTestImpl implements HttpTest {
 
-	private final Logger LOG = LoggerFactory.getLogger(getClass());
+	protected final Logger LOG = LoggerFactory.getLogger(this.getClass());
 
 	private final String host;
 	private final String scheme;
 	private final String context;
 	private final int port;
 	private final ObjectMapper mapper = new ObjectMapper();
-	private final HttpContext httpContext;
+	private final BasicHttpContext httpClientContext;
+	private CloseableHttpClient httpclient;
+
+	public static final HttpTest httpTest = SingletonHelper.build();
 	
-	public HttpTestImpl() {
+	private HttpTestImpl() {
 		LOG.debug("HttpTestImpl instance created.");
 		Properties httpProperties = new Properties();
 		try {
 			httpProperties.load(this.getClass().getClassLoader()
 					.getResourceAsStream("http-config.properties"));
 		} catch (IOException e) { 
-			e.printStackTrace();
+			LOG.error("Error: {}", e.getMessage(), e);
 		}
 		host = httpProperties.getProperty("host", "localhost");
 		scheme = httpProperties.getProperty("scheme", "http");
@@ -62,8 +63,17 @@ public class HttpTestImpl implements HttpTest {
 		port = Integer.parseInt(httpProperties.getProperty("port", "6060"));
 		
 		CookieStore cookieStore = new BasicCookieStore();
-		httpContext = new BasicHttpContext();
-		httpContext.setAttribute(ClientContext.COOKIE_STORE, cookieStore);
+		httpClientContext = new BasicHttpContext();
+		httpClientContext.setAttribute(HttpClientContext.COOKIE_STORE, cookieStore);
+		httpclient = HttpClients.createDefault();
+	}
+	
+	static class SingletonHelper{
+
+		public static HttpTest build() {
+			return new HttpTestImpl();
+		}
+		
 	}
 
 	/*
@@ -71,13 +81,13 @@ public class HttpTestImpl implements HttpTest {
 	 * 
 	 * @see org.asn.web_test.api.HttpTest#doPlainPost(java.util.List)
 	 */
-	public String doPlainPost(String path, NameValuePair[] urlParameters) {
-		CloseableHttpClient httpclient = HttpClients.createDefault();
-		
+	public String doPlainPost(String path, NameValuePair[] urlParameters) {		
 		try {
-			URI uri = buildUri(path, urlParameters);
-			LOG.debug("performing plain post to {}", uri.toURL());
+			URI uri = buildUri(path, urlParameters);			
 			HttpPost httpget = new HttpPost(uri);
+			LOG.debug("performing JSON request..");
+			LOG.debug("URL: {}",httpget.getRequestLine());
+			LOG.debug("Request Data: {}",urlParameters);
 			// Create a custom response handler
             ResponseHandler<String> responseHandler = new ResponseHandler<String>() {
             	
@@ -86,28 +96,20 @@ public class HttpTestImpl implements HttpTest {
                     int status = response.getStatusLine().getStatusCode();
                     if (status >= 200 && status < 300) {
                         HttpEntity entity = response.getEntity();
-                        return entity != null ? EntityUtils.toString(entity) : null;
+                        String responseReceived = entity != null ? EntityUtils.toString(entity) : null;
+                        LOG.debug("Response Data: {}",responseReceived);
+                        return responseReceived;
                     } else {
-                    	StringBuffer buffer = new StringBuffer("Unexpected response status: ");
-                    	buffer.append(status);
-                    	buffer.append(" ");
-                    	buffer.append(response.getStatusLine().getReasonPhrase());
-                        throw new ClientProtocolException(buffer.toString());
+                    	String msg = buildExpMsg(status, response.getStatusLine().getReasonPhrase());                    	
+                        throw new ClientProtocolException(msg);
                     }
                 }
-
             };
 						
-			return httpclient.execute(httpget, responseHandler, httpContext);
+			return httpclient.execute(httpget, responseHandler, httpClientContext);
 
 		} catch (IOException | URISyntaxException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				httpclient.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+			LOG.error("Error: {}", e.getMessage(), e);
 		}
 		return null;
 	}
@@ -118,8 +120,7 @@ public class HttpTestImpl implements HttpTest {
 	 * @see org.asn.web_test.api.HttpTest#doJsonPost(java.lang.Object)
 	 */
 	public <T> T doJsonPost(String path, Object requestBody, final Class<T> entityClass) {
-		T response = null;	
-		CloseableHttpClient httpclient = HttpClients.createDefault();		
+		T response = null;		
 		try {
 			URI uri = buildUri(path);
 			
@@ -128,13 +129,12 @@ public class HttpTestImpl implements HttpTest {
 			httppost.setHeader("Content-type", "application/json");
 			httppost.setHeader("Accept", "application/json");
 			
-			String jsonStr = mapper.writer().writeValueAsString(requestBody);			
-			LOG.debug("data: {}", jsonStr);
+			String jsonStr = mapper.writer().writeValueAsString(requestBody);						
 			// add the JSON as a StringEntity 
-			httppost.setEntity(new StringEntity(jsonStr));					
-			
-			LOG.debug("performing JSON request: {}", httppost.getRequestLine());
-			
+			httppost.setEntity(new StringEntity(jsonStr));								
+			LOG.debug("performing JSON request..");
+			LOG.debug("URL: {}",httppost.getRequestLine());
+			LOG.debug("Request Data: {}",jsonStr);
 			// Create a custom response handler
             ResponseHandler<T> responseHandler = new ResponseHandler<T>() {
             	
@@ -143,28 +143,21 @@ public class HttpTestImpl implements HttpTest {
                     int status = response.getStatusLine().getStatusCode();
                     if (status >= 200 && status < 300) {
                         HttpEntity entity = response.getEntity();
-                        return entity != null ? (T)mapper.readValue(EntityUtils.toString(entity), entityClass) : null;
-                    } else {
-                    	StringBuffer buffer = new StringBuffer("Unexpected response status: ");
-                    	buffer.append(status);
-                    	buffer.append(" ");
-                    	buffer.append(response.getStatusLine().getReasonPhrase());
-                        throw new ClientProtocolException(buffer.toString());
+                        String responseReceived = entity != null ? EntityUtils.toString(entity) : null;
+                        LOG.debug("Response Data: {}",responseReceived);                        
+                        return responseReceived != null ? (T)mapper.readValue(responseReceived, entityClass) : null;
+                    } else {                    	
+                    	String msg = buildExpMsg(status, response.getStatusLine().getReasonPhrase());                    	
+                        throw new ClientProtocolException(msg);
                     }
                 }
 
             };
 			
-			response = (T) httpclient.execute(httppost, responseHandler, httpContext);			
+			response = (T) httpclient.execute(httppost, responseHandler, httpClientContext);			
 
 		} catch (IOException | URISyntaxException e) {
 			LOG.error("Request Failed! {}", e.getMessage(), e);
-		} finally {
-			try {
-				httpclient.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
 		}
 		return response;
 	}
@@ -174,14 +167,13 @@ public class HttpTestImpl implements HttpTest {
 	 * 
 	 * @see org.asn.web_test.api.HttpTest#doPlainGet(java.util.List)
 	 */
-	public String doPlainGet(String path, NameValuePair[] urlParameters) {
-		CloseableHttpClient httpclient = HttpClients.createDefault();
-		
+	public String doPlainGet(String path, NameValuePair[] urlParameters) {		
 		try {
 			URI uri = buildUri(path, urlParameters);
-			LOG.debug("performing plain get to {}", uri.toURL());
 			HttpGet httpget = new HttpGet(uri);
-			
+			LOG.debug("performing JSON request..");
+			LOG.debug("URL: {}",httpget.getRequestLine());
+			LOG.debug("Request Data: {}",urlParameters);
 			// Create a custom response handler
             ResponseHandler<String> responseHandler = new ResponseHandler<String>() {
             	
@@ -190,13 +182,12 @@ public class HttpTestImpl implements HttpTest {
                     int status = response.getStatusLine().getStatusCode();
                     if (status >= 200 && status < 300) {
                         HttpEntity entity = response.getEntity();
-                        return entity != null ? EntityUtils.toString(entity) : null;
+                        String responseReceived = entity != null ? EntityUtils.toString(entity) : null;
+                        LOG.debug("Response Data: {}",responseReceived);
+                        return responseReceived;                        
                     } else {
-                    	StringBuffer buffer = new StringBuffer("Unexpected response status: ");
-                    	buffer.append(status);
-                    	buffer.append(" ");
-                    	buffer.append(response.getStatusLine().getReasonPhrase());
-                        throw new ClientProtocolException(buffer.toString());
+                    	String msg = buildExpMsg(status, response.getStatusLine().getReasonPhrase());                    	
+                        throw new ClientProtocolException(msg);
                     }
                 }
 
@@ -205,13 +196,7 @@ public class HttpTestImpl implements HttpTest {
 			return httpclient.execute(httpget, responseHandler);
 
 		} catch (IOException | URISyntaxException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				httpclient.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+			LOG.error("Error: {}", e.getMessage(), e);
 		}
 		return null;
 	}
@@ -221,14 +206,14 @@ public class HttpTestImpl implements HttpTest {
 	 * 
 	 * @see org.asn.web_test.api.HttpTest#doJsonGet(java.util.List)
 	 */
-	public <T> T doJsonGet(String path, NameValuePair[] urlParameters, final Class<T> entityClass) {
-		CloseableHttpClient httpclient = HttpClients.createDefault();		
+	public <T> T doJsonGet(String path, NameValuePair[] urlParameters, final Class<T> entityClass) {		
 		T response = null;		
 		try {
-			URI uri = buildUri(path, urlParameters);
-			LOG.debug("performing json GET: {}", uri.toURL());
+			URI uri = buildUri(path, urlParameters);			
 			HttpGet httpget = new HttpGet(uri);
-			
+			LOG.debug("performing JSON request..");
+			LOG.debug("URL: {}",httpget.getRequestLine());
+			LOG.debug("Request Data: {}",urlParameters);
 			// Create a custom response handler
             ResponseHandler<T> responseHandler = new ResponseHandler<T>() {
             	
@@ -237,28 +222,21 @@ public class HttpTestImpl implements HttpTest {
                     int status = response.getStatusLine().getStatusCode();
                     if (status >= 200 && status < 300) {
                         HttpEntity entity = response.getEntity();
-                        return entity != null ? (T)mapper.readValue(EntityUtils.toString(entity), entityClass) : null;
+                        String responseReceived = entity != null ? EntityUtils.toString(entity) : null;
+                        LOG.debug("Response Data: {}",responseReceived);                        
+                        return responseReceived != null ? (T)mapper.readValue(responseReceived, entityClass) : null;
                     } else {
-                    	StringBuffer buffer = new StringBuffer("Unexpected response status: ");
-                    	buffer.append(status);
-                    	buffer.append(" ");
-                    	buffer.append(response.getStatusLine().getReasonPhrase());
-                        throw new ClientProtocolException(buffer.toString());
+                    	String msg = buildExpMsg(status, response.getStatusLine().getReasonPhrase());                    	
+                        throw new ClientProtocolException(msg);
                     }
                 }
 
             };
 			
-			response = (T) httpclient.execute(httpget, responseHandler, httpContext);	
+			response = (T) httpclient.execute(httpget, responseHandler, httpClientContext);	
 
 		} catch (IOException | URISyntaxException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				httpclient.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+			LOG.error("Error: {}", e.getMessage(), e);
 		}
 		return response;
 	}
@@ -275,8 +253,7 @@ public class HttpTestImpl implements HttpTest {
 
 	@Override
 	public String doJsonPost(String path, Object data) {
-		String response = null;
-		CloseableHttpClient httpclient = HttpClients.createDefault();		
+		String response = null;		
 		try {
 			URI uri = buildUri(path);
 			
@@ -285,13 +262,13 @@ public class HttpTestImpl implements HttpTest {
 			httppost.setHeader("Content-type", "application/json");
 			httppost.setHeader("Accept", "application/json");
 			
-			String jsonStr = (data!=null)? mapper.writer().writeValueAsString(data) : "";			
-			LOG.debug("data: {}", jsonStr);
+			String jsonStr = (data!=null)? mapper.writer().writeValueAsString(data) : "";						
 			// add the JSON as a StringEntity 
 			httppost.setEntity(new StringEntity(jsonStr));					
 			
-			LOG.debug("performing JSON request: {}", httppost.getRequestLine());
-			
+			LOG.debug("performing JSON request..");
+			LOG.debug("URL: {}",httppost.getRequestLine());
+			LOG.debug("Request Data: {}",jsonStr);
 			// Create a custom response handler
 			ResponseHandler<String> responseHandler = new ResponseHandler<String>() {
 				
@@ -300,60 +277,36 @@ public class HttpTestImpl implements HttpTest {
 					int status = response.getStatusLine().getStatusCode();
 					if (status >= 200 && status < 300) {
 						HttpEntity entity = response.getEntity();
-						return entity != null ? EntityUtils.toString(entity) : null;
+						String responseReceived = entity != null ? EntityUtils.toString(entity) : null;
+                        LOG.debug("Response Data: {}",responseReceived);
+                        return responseReceived;						
 					} else {
-						StringBuffer buffer = new StringBuffer("Unexpected response status: ");
-						buffer.append(status);
-						buffer.append(" ");
-						buffer.append(response.getStatusLine().getReasonPhrase());
-						throw new ClientProtocolException(buffer.toString());
+						String msg = buildExpMsg(status, response.getStatusLine().getReasonPhrase());                    	
+                        throw new ClientProtocolException(msg);
 					}
 				}
 				
 			};
 			
-			response = httpclient.execute(httppost, responseHandler, httpContext);			
+			response = httpclient.execute(httppost, responseHandler, httpClientContext
+					);			
 			
 		} catch (IOException | URISyntaxException e) {
 			LOG.error("Request Failed! {}", e.getMessage(), e);
-		} finally {
-			try {
-				httpclient.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
 		}
 		return response;
 	}
+
+	/**Helper method to build exception message*/
+	protected String buildExpMsg(int status, String reasonPhrase) {
+		return MessageFormat.format("Unexpected response status: {0}, Reason: {1}", status, reasonPhrase);
+	}
 	
-	// helper method to convert InputStream to String
-	/*private String getStringFromInputStream(InputStream is) {
-
-		BufferedReader br = null;
-		StringBuilder sb = new StringBuilder();
-
-		String line;
+	public void shutDown(){
 		try {
-
-			br = new BufferedReader(new InputStreamReader(is));
-			while ((line = br.readLine()) != null) {
-				sb.append(line);
-			}
-
+			httpclient.close();
 		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			if (br != null) {
-				try {
-					br.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-
-		return sb.toString();
-
-	}*/
-
+			LOG.error("Error! {}", e.getMessage(), e);
+		}		
+	}
 }
